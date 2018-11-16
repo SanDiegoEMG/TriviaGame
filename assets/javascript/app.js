4a7d028175855d86dfc8587a4bf3f8859a924c66
SD_emg <sandiegoemg@gmail.com>
2018-11-15 21:32:31 -0800

    timer working! and navbar still affixing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,40 @@
-$("#empty").append("<p>" + "What is going on here" + "</p>");
+// $("#empty").append("<p>" + "What is going on here" + "</p>");
 
 
+var interval;
+
+function countdown() {
+  clearInterval(interval);
+  interval = setInterval( function() {
+      var timer = $('.js-timeout').html();
+      timer = timer.split(':');
+      var minutes = timer[0];
+      var seconds = timer[1];
+      seconds -= 1;
+      if (minutes < 0) return;
+      else if (seconds < 0 && minutes != 0) {
+          minutes -= 1;
+          seconds = 59;
+      }
+      else if (seconds < 10 && length.seconds != 2) seconds = '0' + seconds;
+
+      $('.js-timeout').html(minutes + ':' + seconds);
+
+      if (minutes == 0 && seconds == 0) clearInterval(interval);
+  }, 1000);
+}
+
+$('#js-startTimer').click(function () {
+  console.log("start clicked");
+  $('.js-timeout').text("1:00");
+  countdown();
+});
+
+$('#js-resetTimer').click(function () {
+    console.log("reset clicked");
+  $('.js-timeout').text("1:00");
+  clearInterval(interval);
+});
 
 
 
